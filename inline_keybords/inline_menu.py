5274bf9e43eed2c29654494e548dfa857de45405
aonopko <3052818476Qa_>
2022-07-18 14:22:25 -0400

    connect to db with gino

--- inline_keybords/inline_menu.py
+++ inline_keybords/inline_menu.py
@@ -1,9 +1,16 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from inline_keybords.callbacks import route_callback
 
-menu_kb = InlineKeyboardMarkup(row_width=2,
+route_kb = InlineKeyboardMarkup(row_width=2,
                                inline_keybords=[
                                    [
-                                       InlineKeyboardButton(text="Маршрут",
-                                                            callback_data=route_callback.new())
+                                       InlineKeyboardButton(text="Початкові дані",
+                                                            callback_data=route_callback.new(
+                                                                initial_readings="initial_readings")
+                                                            ),
+                                       InlineKeyboardButton(text="Кінцеві дані",
+                                                            callback_data=route_callback.new(
+                                                                final_readings="final_readings"
+                                                            ))
                                    ]
                                ])
