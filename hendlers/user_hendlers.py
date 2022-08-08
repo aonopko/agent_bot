dfc9f3b0ef35eda75143a65ef90c939f66922876
aonopko <3052818476Qa_>
2022-08-08 15:20:46 -0400

    add inline button

--- hendlers/user_hendlers.py
+++ hendlers/user_hendlers.py
@@ -1,5 +1,6 @@
 from aiogram import types
 from keybords.reply_keybords import menu
+from inline_keybords.inline_menu import agent_kb
 from load_bot import dp
 
 
@@ -7,3 +8,8 @@ from load_bot import dp
 async def start(message: types.Message):
     text = message.text
     await message.answer(text, reply_markup=menu)
+
+@dp.message_handler(text="Агент")
+async def start(message: types.Message):
+    text = message.text
+    await message.answer(text, reply_markup=agent_kb)
--- inline_keybords/inline_menu.py
+++ inline_keybords/inline_menu.py
@@ -1,16 +1,9 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from inline_keybords.callbacks import agent_callback
 
-agent_kb = InlineKeyboardMarkup(row_width=2,
-                               inline_keyboard=[
+
+agent_kb = InlineKeyboardMarkup(row_width=2, inline_keyboard=[
                                    [
-                                       InlineKeyboardButton(text="Додати агента",
-                                                            callback_data=agent_callback.new(
-                                                                add_agent="add_agent")
-                                                            ),
-                                       InlineKeyboardButton(text="Видалити агента",
-                                                            callback_data=agent_callback.new(
-                                                                del_agent="del_agent"
-                                                            ))
+                                       InlineKeyboardButton(text="Додати агента", url="https://github.com/"),
+                                       InlineKeyboardButton(text="Видалити агента", url="https://github.com/")
                                    ]
                                ])
