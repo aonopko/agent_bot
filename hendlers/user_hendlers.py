378eed8f288d1280dc8dacc660f1d51970b6eb93
aonopko <3052818476Qa_>
2022-08-11 15:54:40 -0400

    work with inline button

--- hendlers/user_hendlers.py
+++ hendlers/user_hendlers.py
@@ -1,6 +1,9 @@
 from aiogram import types
+from aiogram.types import CallbackQuery
+
 from keybords.reply_keybords import menu
 from inline_keybords.inline_menu import agent_kb
+from inline_keybords.callbacks import agent_callback
 from load_bot import dp
 
 
@@ -9,7 +12,15 @@ async def start(message: types.Message):
     text = message.text
     await message.answer(text, reply_markup=menu)
 
+
 @dp.message_handler(text="Агент")
 async def start(message: types.Message):
     text = message.text
     await message.answer(text, reply_markup=agent_kb)
+
+
+@dp.callback_query_handler(agent_callback.filter(id_agent=123))
+async def add_agent(call: CallbackQuery, callback_data: dict):
+    await call.answer(text="Добавляем")
+    agent_name = callback_data.get("agent_name")
+    await call.message.answer(f"{agent_name}")
