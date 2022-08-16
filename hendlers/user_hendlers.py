a688682e27c6f9ccfb278178619c74bfd994167b
aonopko <3052818476Qa_>
2022-08-16 15:52:47 -0400

    work with inline

--- hendlers/user_hendlers.py
+++ hendlers/user_hendlers.py
@@ -1,8 +1,10 @@
 from aiogram import types
+from aiogram.dispatcher.filters import Text
 from aiogram.types import CallbackQuery
 
+from states.route_state import Agent
 from keybords.reply_keybords import menu
-from inline_keybords.inline_menu import agent_kb
+#from inline_keybords.inline_menu import agent_kb
 from inline_keybords.callbacks import agent_callback
 from load_bot import dp
 
@@ -13,13 +15,22 @@ async def start(message: types.Message):
     await message.answer(text, reply_markup=menu)
 
 
+def get_agent_kb():
+    buttons = [
+        types.InlineKeyboardButton(text="Додати агента", callback_data="add_agent")
+    ]
+    keyboard = types.InlineKeyboardMarkup(row_width=2)
+    keyboard.add(*buttons)
+    return keyboard
+
+
 @dp.message_handler(text="Агент")
 async def start(message: types.Message):
     text = message.text
-    await message.answer(text, reply_markup=agent_kb)
+    await message.answer(text, reply_markup=get_agent_kb())
 
 
-@dp.callback_query_handler(agent_callback.filter())
+@dp.callback_query_handler(Text(startswith="add_agent"))
 async def add_agent(call: types.CallbackQuery, callback_data: dict):
     agent_name = callback_data.get("id_agent")
     await call.message.answer(f"{agent_name}")
--- inline_keybords/inline_menu.py
+++ inline_keybords/inline_menu.py
@@ -2,11 +2,11 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 from .callbacks import agent_callback
 
-
+id_agent = 123
 agent_kb = InlineKeyboardMarkup(row_width=2, inline_keyboard=[
                                    [
                                        InlineKeyboardButton(text="Додати агента",
-                                                            callback_data=agent_callback.new(id_agent=123,
+                                                            callback_data=agent_callback.new(id_agent="Агент",
                                                                                              agent_name="Andrey"))
                                    ]
                                ])
