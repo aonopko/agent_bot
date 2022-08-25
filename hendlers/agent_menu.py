d14d9dd6c865cbc10a3f360b735067cc344171dd
aonopko <3052818476Qa_>
2022-08-25 16:07:05 -0400

    work with button add_agent

--- hendlers/agent_menu.py
+++ hendlers/agent_menu.py
@@ -1,6 +1,7 @@
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
+from asyncpg.exceptions import UniqueViolationError
 
 from db.db_commands import add_agent
 from states.route_state import AgentState
@@ -34,24 +35,31 @@ async def start(message: types.Message):
 async def add_agent_name(call: types.CallbackQuery):
     await call.message.answer("Введіть Ім'я")
     await call.answer()
-
     await AgentState.agent_name.set()
 
 
 @dp.message_handler(state=AgentState.agent_name)
 async def add_id_agent(message: types.Message, state: FSMContext):
-    agent_name = message.text
     async with state.proxy() as data:
-        data["agent_name"] = agent_name
-
-    await AgentState.id_agent.set()
-    await message.answer("Потрібно ввести ОК для завершення")
+        data["agent_name"] = message.text
+        await AgentState.id_agent.set()
+        button = types.InlineKeyboardButton(text="OK",
+                                            callback_data="OK")
+        await message.answer(f"Натисніть {button} для завершення")
 
 
 @dp.message_handler(state=AgentState.id_agent)
 async def get_id_name(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data["id_agent"] = message.from_user.id
-    await add_agent(agent_id=data.get("id_agent"),
-                    agent_name=data.get("agent_name"))
-    await message.answer("Ваш id додано")
+        get_id = data.get("id_agent")
+    try:
+        await add_agent(agent_id=data.get("id_agent"),
+                        agent_name=data.get("agent_name"))
+    except UniqueViolationError:
+        await message.answer(f"УВАГА\n"
+                             f"Агент з id {get_id} існує")
+    else:
+        await message.answer("Ваш id додано")
+
+    await state.finish()
