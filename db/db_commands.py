d5d9228d002a73b170e5a71eb3a81d728a49aec5
aonopko <3052818476Qa_>
2022-09-01 15:02:31 -0400

    work with func show_data

--- db/db_commands.py
+++ db/db_commands.py
@@ -12,5 +12,4 @@ async def del_agent(agent_id):
 
 
 async def show_data_agent(agent_id):
-    await Agent.query.where(Agent.agent_id == agent_id).gino.all()
-
+    await Agent.query.where(Agent.agent_id == agent_id).gino.first()
