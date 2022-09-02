7347833da1312bc4029f0635a4b3247918185ecc
aonopko <3052818476Qa_>
2022-09-02 14:53:06 -0400

    work with func show_data

--- hendlers/show_data_agent.py
+++ hendlers/show_data_agent.py
@@ -9,5 +9,5 @@ from db.db_commands import show_data_agent
 async def show_dat_agent(call: types.CallbackQuery):
     id_agent = call.from_user.id
     show_data = await show_data_agent(agent_id=id_agent)
-    await call.message.answer(f"Ім'я {show_data} ок")
+    await call.message.answer(f"Ім'я {show_data} ok and ok")
     await call.answer()
