014bc490f8cfdd9e2346ee55405ad52c30d0c7b7
aonopko <aonopko.rpu@gmail.com>
2022-09-18 13:55:32 -0400

    create django project

--- load_bot.py
+++ load_bot.py
@@ -13,16 +13,9 @@ async def on_startup(dp):
     logger.info('Connection complete')
 
 
-def setup_django():
-    logger.info("CREATE DJANGO CONNECTION")
-    os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE",
-        "telegrambot.django_project.settings"
-    )
-    os.environ.update({"DJANGO_ALLOW_ASYNC_UNSAFE": "true"})
-    logger.info("DJANGO WAS CONNECT")
+
 
 
 if __name__ == '__main__':
-    setup_django()
+    #setup_django()
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
