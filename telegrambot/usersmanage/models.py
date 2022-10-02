6a25e042ad533b264ea909b3677b255c77f5110d
aonopko <aonopko.rpu@gmail.com>
2022-10-02 14:01:21 -0400

    work with models

--- telegrambot/usersmanage/models.py
+++ telegrambot/usersmanage/models.py
@@ -1,3 +1,8 @@
+
 from django.db import models
 
-class Route()
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_name = models.CharField(max_length=150)
+    telegram_id = models.IntegerField()
